f87519d022dd9f1563578ae650b455a5a1055e32
bruck <augustin.bruck@epfl.ch>
2026-04-19 17:18:08 +0200

    knn test

--- src/methods/knn.py
+++ src/methods/knn.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from src.utils import normalize_fn
 
 class KNN(object):
     """
@@ -29,11 +29,27 @@ class KNN(object):
             pred_labels (np.array): labels of shape (N,)
         """
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        pred_labels = []
+
+        self.training_labels
+        self.means = training_data.mean(0,keepdims=True)
+        self.stds  = training_data.std(0,keepdims=True)
+
+        self.normalized_space = normalize_fn(training_data, self.means, self.stds)
+
+        for i in range(self.normalized_space.shape[0]):
+            point = self.normalized_space[i]
+            diff = self.normalized_space - point
+            squared = diff ** 2
+            distances = np.sqrt(np.sum(squared, axis=1))
+            distances[i] = np.inf
+            indices = np.argsort(distances)[:self.k]
+            k_nearest_neighbour = training_labels[indices]
+            most_common_label = np.argmax(np.bincount(k_nearest_neighbour))
+            pred_labels.append(most_common_label)
+            # add weights possibly
+
+        pred_labels = np.array(pred_labels)
         return pred_labels
 
     def predict(self, test_data):
@@ -45,9 +61,27 @@ class KNN(object):
         Returns:
             test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        test_labels = []
+
+        test_data = normalize_fn(test_data, self.means, self.stds)
+
+        for i in range(test_data.shape[0]):
+            point = test_data[i]
+            diff = self.normalized_space - point
+            squared = diff**2
+            distances = np.sqrt(np.sum(squared, axis=1))
+            indices = np.argsort(distances)[:self.k]
+
+            
+            k_nearest_neighbour = self.training_labels[indices]
+            most_common_label = np.argmax(np.bincount(k_nearest_neighbour))
+            test_labels.append(most_common_label)
+
+            ## question here for tmrw
+            ## it is asked in pdf that knn should support regression and classification
+            ## here i've implemented classficiation
+            ## for regression we look at k nearest neighbours then take their mean
+            ## but this is taken from train_labels_reg which is not provided here 
+            ## depends on task_kind, should i still implement it, or it's hard coded ?
+        test_labels = np.array(test_labels)
         return test_labels
