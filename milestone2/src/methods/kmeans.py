6a4ac361e86df75f44f94d3f2640fe8385bf63b9
Roywass <roywass1204@gmail.com>
2026-05-22 18:26:31 +0200

    K-Means

--- milestone2/src/methods/kmeans.py
+++ milestone2/src/methods/kmeans.py
@@ -18,7 +18,11 @@ class KMeans(object):
             max_iters (int): maximum number of iterations
         """
 
-        ### WRITE YOUR CODE HERE
+        self.K = K
+        self.max_iters = max_iters
+
+        self.centers = None
+        self.cluster_center_label = None
 
 
     def init_centers(self, data):
@@ -32,7 +36,12 @@ class KMeans(object):
             centers: array of shape (KxD) of initial cluster centers
         """
 
-        ### WRITE YOUR CODE HERE
+        # We choose K random indices from the N points, without replacement
+        indices = np.random.choice(data.shape[0], self.K, replace=False)
+        centers = data[indices]
+        
+        return centers
+
 
     def compute_distance(self, data, centers):
         """
@@ -45,7 +54,10 @@ class KMeans(object):
             distances: array of shape (N, K) with the distances between the N points and the K clusters.
         """
 
-        ### WRITE YOUR CODE HERE
+        diff = data[:, np.newaxis, :] - centers[np.newaxis, :, :]
+        distances = np.sqrt(np.sum(diff ** 2, axis=2))
+        
+        return distances
 
 
     def find_closest_cluster(self, distances):
@@ -58,7 +70,11 @@ class KMeans(object):
             cluster_assignments: array of shape (N,), cluster assignment of each datapoint, which are an integer between 0 and K-1.
         """
 
-        ### WRITE YOUR CODE HERE
+        # We take the index of the minimum on the cluster axis
+        cluster_assignments = np.argmin(distances, axis=1)
+        
+        return cluster_assignments
+
 
 
     def compute_centers(self, data, cluster_assignments):
@@ -73,7 +89,22 @@ class KMeans(object):
             centers: the new centers of each cluster, shape is (K,D) where K is the number of clusters, D the number of features
         """
 
-        ### WRITE YOUR CODE HERE
+        D = data.shape[1]
+        centers = np.zeros((self.K, D))
+
+        for k in range(self.K):
+            # All points assigned to cluster k
+            points_in_cluster = data[cluster_assignments == k]
+
+            if len(points_in_cluster) == 0:
+                # Empty cluster: random point
+                centers[k] = data[np.random.randint(0, data.shape[0])]
+            else:
+                # Center = average of the points in the cluster
+                centers[k] = np.mean(points_in_cluster, axis=0)
+
+        return centers
+
 
 
     def k_means(self, data, max_iter=100):
@@ -88,7 +119,24 @@ class KMeans(object):
             cluster_assignments (array): shape (N,) final cluster assignment for each data point.
         """
 
-        ### WRITE YOUR CODE HERE
+        # Initialize the centers
+        centers = self.init_centers(data)
+
+        for i in range(max_iter):
+            # Calculate the distances and assign to the nearest cluster
+            distances = self.compute_distance(data, centers)
+            cluster_assignments = self.find_closest_cluster(distances)
+
+            # Calculate the new centers
+            new_centers = self.compute_centers(data, cluster_assignments)
+
+            # Check convergence
+            if np.allclose(centers, new_centers):
+                break
+
+            centers = new_centers
+
+        return centers, cluster_assignments
 
     def assign_labels_to_centers(self, centers, cluster_assignments, true_labels):
         """
@@ -102,7 +150,23 @@ class KMeans(object):
             cluster_center_label: array of shape (K,), the labels of the cluster centers
         """
 
-        ### WRITE YOUR CODE HERE
+        cluster_center_label = np.zeros(self.K, dtype=int)
+
+        for k in range(self.K):
+            # Labels of points belonging to cluster k
+            labels_in_cluster = true_labels[cluster_assignments == k]
+
+            if len(labels_in_cluster) == 0:
+                # Empty cluster: label 0 by default
+                cluster_center_label[k] = 0
+            else:
+                # We take the most represented label
+                vals, counts = np.unique(labels_in_cluster, return_counts=True) 
+                index_max = np.argmax(counts)
+
+                cluster_center_label[k] = int(vals[index_max])
+
+        return cluster_center_label
 
     def predict_with_centers(self, data, centers, cluster_center_label):
         """
@@ -118,7 +182,16 @@ class KMeans(object):
             new_labels: array of shape (N,), the labels assigned to each data point after clustering, via k-means.
         """
 
-        ### WRITE YOUR CODE HERE
+        # Calculate the distances to the final centers
+        distances = self.compute_distance(data, centers)
+
+        # Assign to the nearest center
+        cluster_assignments = self.find_closest_cluster(distances)
+
+        # Retrieve the label of the assigned cluster
+        new_labels = cluster_center_label[cluster_assignments]
+
+        return new_labels
 
     def fit(self, training_data, training_labels):
         """
@@ -133,7 +206,16 @@ class KMeans(object):
         Returns:
             pred_labels (array): labels of shape (N,)
         """
-        ### WRITE YOUR CODE HERE
+        # Unsupervised Clustering
+        self.centers, cluster_assignments = self.k_means(training_data, max_iter=self.max_iters)
+
+        # Assign a label to each center
+        self.cluster_center_label = self.assign_labels_to_centers(self.centers, cluster_assignments, training_labels)
+
+        # Prediction based on training data
+        pred_labels = self.predict_with_centers(training_data, self.centers, self.cluster_center_label)
+
+        return pred_labels
 
     def predict(self, test_data):
         """
@@ -147,4 +229,7 @@ class KMeans(object):
         Returns:
             pred_labels (array): labels of shape (N,)
         """
-        ### WRITE YOUR CODE HERE
+        
+        pred_labels = self.predict_with_centers(test_data, self.centers, self.cluster_center_label)
+        
+        return pred_labels
